431dac082243c4b9589f2185f6f79ba7ccfdbff2
Matthew2025-byte <mdstapleton2020@outlook.com>
2026-03-06 11:51:04 -0800

    cli uses python curses

--- client.py
+++ client.py
@@ -1,9 +1,6 @@
-import argparse, socket, ipaddress, threading
-
-def input_thread(sock):
-    while True:
-        msg = input("message: ")
-        sock.sendall(msg.encode())
+import argparse, socket, ipaddress
+from curses import wrapper
+import curses
 
 def ipv4_or_localhost(value):
     """Validates that the address given by the user is a valid IPv4 address"""
@@ -33,18 +30,43 @@ def ipv4_or_localhost(value):
         raise argparse.ArgumentError("Host must be a valid IPv4 address")
     
     return (str(ip), port)
-            
+
+      
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('address', type=ipv4_or_localhost)
 
 args = parser.parse_args()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(args.address)
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sock.connect(args.address)
 
-threading.Thread(target=input_thread, args=(sock,), daemon=True).start()
+try:
+    stdscr = curses.initscr()
+    stdscr.nodelay(True)
+    stdscr.keypad(True)
+    height, width = stdscr.getmaxyx()
 
-while True:
-    data = sock.recv(1024)
-    print(f"\rRecieved: {data}\nmessage: ", end="")
+    msgHistory_win = curses.newwin(height - 3, width, 0, 0)
+    input_win = curses.newwin(3, width, height - 3, 0)
+
+    while True:
+        key = stdscr.getch()
+        if key != -1:
+            # display the size of the terminal (debug purposes)
+            if key == curses.KEY_RESIZE:
+                height, width = stdscr.getmaxyx()
+
+                msgHistory_win.resize(height - 3, width)
+                msgHistory_win.mvwin(0, 0)
+                input_win.resize(3, width)
+                input_win.mvwin(height - 3, 0)
+
+                msgHistory_win.clear()
+                msgHistory_win.addstr(0, 0, f"{width}x{height}")
+                msgHistory_win.refresh()
+            if key == ord('q'):
+                break
+finally:
+    stdscr.keypad(False)
+    curses.endwin()
